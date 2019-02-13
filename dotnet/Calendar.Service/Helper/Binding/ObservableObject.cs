12bce9b15781ab561eb2357d83f1e33a6eaa0806
Revolt27 <kuckushckin.alex@yandex.ru>
2019-02-13 21:26:57 +0300

    Добавлена библиотека Service, добавлена реализация MVVM паттерна.

---
+++ dotnet/Calendar.Service/Helper/Binding/ObservableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Calendar.Service.Helper.Binding
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        #region Fields
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        protected virtual void RaiseOnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            if (string.IsNullOrEmpty(prop))
+                throw new ArgumentException("Значение не может быть пустым или NULL");
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion
+
+    }
+}
