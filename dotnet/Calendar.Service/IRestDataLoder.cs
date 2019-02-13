bc9566cab42d009c020b97cb88a33ba101406239
Revolt27 <kuckushckin.alex@yandex.ru>
2019-02-13 22:06:58 +0300

    Добавлен интерфейс и класс будущего сервиса взаимодействия с API

---
+++ dotnet/Calendar.Service/IRestDataLoder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Calendar.Service
+{
+    internal interface IRestDataLoder
+    {
+        #region Methods
+
+        /// <summary>
+        /// Возвращает коллекцию моделей данных.
+        /// </summary>
+        /// <typeparam name="T">Модель данных</typeparam>
+        Task<IEnumerable<T>> GetData<T>() where T : class;
+
+        /// <summary>
+        /// Передаёт модель данных для записи и возвращает результат.
+        /// </summary>
+        /// <param name="data">Модель данных</param>
+        bool SetData<T>(T data) where T : class;
+
+        /// <summary>
+        /// Передаёт модель данных для изменения и возвращает результат.
+        /// </summary>
+        /// <param name="data">Модель данных</param>
+        bool ChangeData<T>(T data) where T : class;
+
+        /// <summary>
+        /// Передаёт модель данных для удаления и возвращает результат.
+        /// </summary>
+        /// <param name="data">Модель данных</param>
+        bool DeleteData<T>(T data) where T : class;
+
+        #endregion
+    }
+}
---
+++ dotnet/Calendar.Service/RestDataLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Calendar.Service
+{
+    public class RestDataLoader : IRestDataLoder
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        /// <param name="url">URL адресс API сервера</param>
+        public RestDataLoader(string url)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool ChangeData<T>(T data) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteData<T>(T data) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetData<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetData<T>(T data) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
